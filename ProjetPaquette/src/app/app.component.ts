6adb72fec88c178bdae4f83cfdb2ae3cc58f0480
karl AFATOLO <49211239+karlAFATOLO@users.noreply.github.com>
2021-11-22 18:32:25 +0100

    pour john et floflo

---
+++ ProjetPaquette/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ProjetPaquette';
+}
+
+export class monApplicationComponent {
+  cheminImage:any = "./Estiam-Logo-Bicolore-RVB-1024.png";
+}
