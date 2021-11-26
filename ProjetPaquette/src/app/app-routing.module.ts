67533f87dcb30f98fac55c85ce5aba3cd64e7665
Yawda <florianayt@gmail.com>
2021-11-26 01:38:38 +0000

    Ajout des pages et des routes dans  le routing  et app.component.html

--- ProjetPaquette/src/app/app-routing.module.ts
+++ ProjetPaquette/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdmissionComponent } from './pages/admission/admission.component';
+import { AlternanceComponent } from './pages/alternance/alternance.component';
+import { CertificationComponent } from './pages/certification/certification.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { EntrepriseComponent } from './pages/entreprise/entreprise.component';
+import { ProgrammeComponent } from './pages/programme/programme.component';
+import { TarifsComponent } from './pages/tarifs/tarifs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contactez-nous', component: ContactComponent },
+  { path: 'tarifs', component: TarifsComponent },
+  { path: 'certifications', component: CertificationComponent },
+  { path: 'entreprise', component: EntrepriseComponent },
+  { path: 'alternance', component: AlternanceComponent },
+  { path: 'programme', component: ProgrammeComponent },
+  { path: 'admission', component: AdmissionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ProjetPaquette/src/app/app.module.ts
+++ ProjetPaquette/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdmissionComponent } from './pages/admission/admission.component';
+import { ProgrammeComponent } from './pages/programme/programme.component';
+import { AlternanceComponent } from './pages/alternance/alternance.component';
+import { EntrepriseComponent } from './pages/entreprise/entreprise.component';
+import { CertificationComponent } from './pages/certification/certification.component';
+import { TarifsComponent } from './pages/tarifs/tarifs.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdmissionComponent,
+    ProgrammeComponent,
+    AlternanceComponent,
+    EntrepriseComponent,
+    CertificationComponent,
+    TarifsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
